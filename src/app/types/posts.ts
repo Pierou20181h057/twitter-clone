b30a3743400a280ad18dcb8e8b3e338d7fc5f047
Piero Liberato <54274115+Pierou20181h057@users.noreply.github.com>
2024-01-29 23:16:59 -0500

    add: Finish proyect

---
+++ src/app/types/posts.ts
@@ -0,0 +1,11 @@
+import { type Database } from '../types/database'
+
+type PostEntity = Database['public']['Tables']['posts']['Row']
+type UserEntity = Database['public']['Tables']['users']['Row']
+
+export type Post = PostEntity & {
+  user: UserEntity
+}
+export interface PostWithUser extends Omit<PostEntity, 'user'> {
+  user: UserEntity
+}
